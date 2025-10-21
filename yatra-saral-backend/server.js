967bcede17b74e422a299d195c9fb8e23edf7493
Rudra Gupta <23rudragupta@gmail.com>
2025-10-21 20:34:09 +0530

    Update server.js
    Updated Nodemailer configuration to use SMTP settings for Gmail.

--- yatra-saral-backend/server.js
+++ yatra-saral-backend/server.js
@@ -24,10 +24,12 @@ client.connect()
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587, // Standard port for TLS
+  secure: false, // TLS requires secure: false
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, 
+    pass: process.env.EMAIL_PASS, // Your App Password
   },
 });
 
